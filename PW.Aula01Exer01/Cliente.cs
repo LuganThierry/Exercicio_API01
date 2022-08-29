00946c7070f75065cade2603de13e5e9099f8186
Lugan Thierry <luganthierry@hotmail.com>
2022-08-29 18:29:24 -0300

    Add project files.

---
+++ PW.Aula01Exer01/Cliente.cs
@@ -0,0 +1,13 @@
+﻿namespace PW.Aula01Exer01
+{
+    public class Cliente
+    {
+        public string Cpf { get; set; }
+
+        public string Nome { get; set; }
+
+        // public DateTime DataNasc { get; set; }
+
+        public int Idade { get; set; }
+    }
+}
---
+++ PW.Aula01Exer01/Controllers/UserController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PW.Aula01Exer01.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        [HttpPost("Criar/{nome}")]
+        public IActionResult Criar(string _cpf, string _nome, int _idade)
+        {
+            var novoUsuario = new Cliente();
+            novoUsuario.Cpf = _cpf;
+            novoUsuario.Nome = _nome;
+            novoUsuario.Idade = _idade;
+
+            return Ok (novoUsuario);
+        }
+
+        [HttpGet("Selecionar/{nome}")]
+        public IActionResult Selecionar(string _nome)
+        {
+            var selecionarUsuario = new Cliente();
+
+            if (selecionarUsuario.Nome == _nome)
+                return Ok (selecionarUsuario);
+            
+        }
+
+        [HttpPut("Atualizar/{nome}")]
+        public IActionResult Atualizar()
+        {
+
+        }
+
+        [HttpDelete("Deletar/{nome}")]
+        public IActionResult Deletar()
+        {
+
+        }
+
+    }
+}
